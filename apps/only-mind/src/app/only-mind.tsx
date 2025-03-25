4326433bbb8a1c27e2a82094a15fb6bb3eef6067
pubuzhixing8 <pubuzhixing@gmail.com>
2025-03-25 21:18:01 +0800

    feat: fix libs errors

--- apps/only-mind/src/app/only-mind.tsx
+++ apps/only-mind/src/app/only-mind.tsx
@@ -1,4 +1,4 @@
-import { Board, BoardChangeData, Wrapper } from '@drawnix/react-board';
+import { Board, BoardChangeData, Wrapper } from '@plait-board/react-board';
 import {
   PlaitBoard,
   PlaitBoardOptions,
@@ -10,11 +10,10 @@ import {
   ThemeColorMode,
   Viewport,
 } from '@plait/core';
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { MindThemeColors, withMind } from '@plait/mind';
 import { withDraw } from '@plait/draw';
 import { withGroup } from '@plait/common';
-import { DrawnixContext, DrawnixState } from './use-drawnix';
 
 export type OnlyMindProps = {
   value: PlaitElement[];
@@ -46,17 +45,6 @@ export const OnlyMind: React.FC<OnlyMindProps> = ({
     themeColors: MindThemeColors,
   };
   const plugins: PlaitPlugin[] = [withDraw, withGroup, withMind];
-  // 添加一个 ref 来追踪组件是否已挂载
-  const isMounted = useRef(false);
-
-  const [appState, setAppState] = useState<DrawnixState>(() => {
-    // TODO: need to consider how to maintenance the pointer state in future
-    return {
-      pointer: PlaitPointerType.hand,
-      isPencilMode: false,
-      openDialog: false,
-    };
-  });
   return (
     <Wrapper
       value={value}
